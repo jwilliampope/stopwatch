0eff72660da5d62611447f260a1e2d8c830dd33a
Jack Pope <57918947+jwilliampope@users.noreply.github.com>
2023-11-04 19:19:24 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@ function timer() {
   let hrs = Math.floor(seconds / 3600)
   let mins = Math.floor((seconds - hrs * 3600) / 60)
   let secs = seconds % 60
-
-  if (secs < 10) secs = "0" + secs
+  
   if (hrs < 10) hrs = "0" + hrs
   if (mins < 10) mins = "0" + mins
+  if (secs < 10) secs = "0" + secs
 
   time_el.innerText = `${hrs}:${mins}:${secs}`
 }
